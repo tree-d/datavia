ab65acd450ad19982eb536632286b456f58038bf
Michael Berg <michael.berg@ufz.de>
2026-04-01 16:31:12 +0200

    refactor name space structure

--- packages/elevation/datavia/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Datavia namespace package for the elevation pipeline."""
-
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
--- packages/soil/datavia/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Namespace package for the soil pipeline extension of datavia."""
-
-# Namespace package marker for soil pipeline
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
